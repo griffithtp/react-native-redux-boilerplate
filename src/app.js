9e7e7df712b709090f2140886dd14c0217fe3ae3
Griffith Tchenpan <griffith.tchenpan@myworkpapers.com>
2017-11-26 15:58:24 +0000

    added Redux boilerplate

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { View } form 'react-navtive';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 const App = () => {
   return (
-    <View>App</View>
+    <Provider store={ createStore(reducers) }>
+      <View>App</View>
+    </Provider>
   )
 }
 export default App;
